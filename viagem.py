100efa16e4cc95baf027495146b5ede3f865df88
Marcos Felipe <marcosfelipemendesrocha@gmail.com>
2024-04-29 15:24:06 -0300

    Pedindo nome para o usuário

--- viagem.py
+++ viagem.py
@@ -1,9 +1,9 @@
 # Solicitar informações ao usuário
 d = float(input("Insira a distância em quilômetros: "))
 v = float(input("Insira a velocidade em quilômetros por hora: "))
-
+m = input("Digite o nome do motorista: ")
 # Calcular o tempo
 t = d / v
 
 # Exibir o resultado
-print(f"O tempo necessário para fazer o percurso é de {t:.2f} horas.")
+print(f"O tempo necessário para fazer o percurso é de {t:.2f} horas. com o motorista {m}")
